2ae25c318a658e5946bc1a8efe588d42bc004f4b
rezamike <michael.sanaiha@gmail.com>
2025-05-24 14:52:30 -0700

    init2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ export default function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/home" element={<MainSite />} />
       </Routes>
     </Router>
   );
